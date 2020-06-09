26ee062e31bc42c99a613213e605b35094f5ead4
JadFatTail <66198878+JadFatTail@users.noreply.github.com>
2020-06-09 22:16:59 +0800

    量化小工具

--- 工具/private_tools.py
+++ 工具/private_tools.py
@@ -2,6 +2,11 @@ import datetime
 import pandas as pd
 import numpy as np
 from jqdata import *
+import datetime
+import pandas as pd
+import numpy as np
+from jqfactor import get_factor_values
+from jqdata import *
 class private_tools():
     '''比较DataFrame'''
     def compare_serice(self, serice_1, serice_2, flag):
@@ -13,8 +18,8 @@ class private_tools():
             compare_flag = (serice_1 < serice_2)
             serice_2[compare_flag] = serice_1[compare_flag]
             return serice_2
-    '''利用采样函数，设定转换周期period_type  转换为周是'W',月'M',季度线'Q',五分钟'5min',12天'12D''''
-    def get_period_date(peroid,start_date, end_date):
+    '''利用采样函数，设定转换周期period_type  转换为周是'W',月'M',季度线'Q',五分钟'5min',12天12D'''
+    def get_period_date(self, peroid,start_date, end_date):
         stock_data = get_price('000001.XSHE',start_date,end_date,'daily',fields=['close'])
         #types = stock_data.index.dtype 查看index的数据类型
         stock_data['date']=stock_data.index #resample按照index自然日采样，获取日期临时值
@@ -24,7 +29,7 @@ class private_tools():
         date_list.insert(0, start_date_0)
         return date_list
     '''过滤次新股票'''
-    def filter_new_stock(stocks, currernt_date, delta = 30 * 6):
+    def filter_new_stock(self, stocks, currernt_date, delta = 30 * 6):
         stockList=[]
         currernt_date = datetime.datetime.strptime(currernt_date, "%Y-%m-%d")
         for stock in stocks:
@@ -32,8 +37,24 @@ class private_tools():
             if start_date < (currernt_date - datetime.timedelta(days = delta)).date():
                 stockList.append(stock)
         return stockList
+    '''
+    交易日位移
+    shift：位移，为避免未来函数，不提供时间后移功能
+    '''
+    def shift_trading_day(self, current_date, shift):
+        stock_data = get_price('000001.XSHE',count = shift * 2, end_date = current_date, frequency = 'daily',fields=['close'])
+        #types = stock_data.index.dtype 查看index的数据类型
+        trade_date = stock_data.index.map(lambda x : x.strftime('%Y-%m-%d')).values.tolist()
+        trade_date_index = trade_date.index(current_date) - shift
+        return trade_date[trade_date_index]
+    '''过滤停牌股票'''
+    def get_nopaued_stock(self, stock_list, cunrrent_date):
+        suspened_info_df = get_price(stock_list, start_date=cunrrent_date, end_date=cunrrent_date, frequency='daily', fields='paused', panel=False)
+        suspened_info_df.set_index('code', inplace = True)
+        suspened_info_df = suspened_info_df[suspened_info_df['paused'] < 1]
+        return suspened_info_df.index.values.tolist()
     '''获取股票池'''
-    def filter_st_stock(stocks, current_date):
+    def filter_st_stock(self, stocks, current_date):
         #剔除ST股
         st_stock_info = get_extras('is_st', stocks, count = 1, end_date = current_date)
         stock_filter = [stock for stock in stocks if not st_stock_info[stock][0]]
@@ -44,7 +65,7 @@ class private_tools():
     series:待处理数据，Series
     n：几个单位的偏离值
     '''
-    def filter_extreme_MAD(series_pre, n):
+    def filter_extreme_MAD(self, series_pre, n):
         median = series_pre.quantile(0.5)
         new_median = ((series_pre - median).abs()).quantile(0.50)
         max_range = median + n * new_median
@@ -74,7 +95,7 @@ class private_tools():
     series_pre:待处理数据，数据类型Series
     ty：标准化类型:1 MinMax,2 Standard,3 maxabs
     '''
-    def standardize(series_pre, ty = 2):
+    def standardize(self, series_pre, ty = 2):
         series_pre_copy = series_pre.dropna().copy()
         if int(ty) == 1:
             series_pre_copy = (series_pre_copy - series_pre_copy.min()) / (series_pre_copy.max() - series_pre_copy.min())
@@ -86,13 +107,19 @@ class private_tools():
         elif ty == 3:
             re = series_pre_copy / (10 ** np.ceil(np.log10(series_pre_copy.abs().max())))
         return re
+    def standardize_df(self, factor_data, ty = 2):
+        result = pd.DataFrame()
+        for factor in factor_data.columns:
+            result[factor] = self.standardize(factor_data[factor], ty = 2)
+        return result
+        
     '''
     缺失值处理，一般情况应该去除drop（how = ‘any’）,但A股数据量本来缺少，最终还是决定以均值替代
     若是行业中因子值为空，则以所有行业均值代替
     某只股票因子值为空，用行业平均值代替，
     依然会有nan，则用所有股票平均值代替
     ''' 
-    def replace_nan_indu(factor_data, stockList, industry_code, date):
+    def replace_nan_indu(self, factor_data, stockList, industry_code, date):
 
         i_Constituent_Stocks = {}
         if isinstance(factor_data, pd.DataFrame):
@@ -122,23 +149,44 @@ class private_tools():
     输出：
     中性化后的因子值series
     ''' 
-    def neutralization(factor, mkt_cap=False, industry=False):
+    def neutralize(self, factor, mkt_cap=False, industry=False):
         y = factor
         if type(mkt_cap) == pd.Series:
             # LnMktCap = mkt_cap.apply(lambda x:math.log(x))
             if industry:  # 行业、市值
-                dummy_industry = get_industry_exposure(factor.index)
+                dummy_industry = self.get_industry_exposure(factor.index)
                 x = pd.concat([mkt_cap, dummy_industry.T], axis=1)
             else:  # 仅市值
                 x = mkt_cap
         elif industry:  # 仅行业
-            dummy_industry = get_industry_exposure(factor.index)
+            dummy_industry = self.get_industry_exposure(factor.index)
             x = dummy_industry.T
         result = sm.OLS(y.astype(float), x.astype(float)).fit()
         return result.resid
+    
+    def neutralize_df(self, factor_df, exclude = None, mkt_cap=False, industry=False):
+        stocks = factor_df.index
+        result = pd.DataFrame()
+        for factor in factor_df.columns:
+            if factor == exclude:
+                result[factor] = factor_df[factor]
+            else:
+                y = factor_df[factor]
+                if type(mkt_cap) == pd.Series:
+                    # LnMktCap = mkt_cap.apply(lambda x:math.log(x))
+                    if industry:  # 行业、市值
+                        dummy_industry = self.get_industry_exposure(stocks)
+                        x = pd.concat([mkt_cap, dummy_industry.T], axis=1)
+                    else:  # 仅市值
+                        x = mkt_cap
+                elif industry:  # 仅行业
+                    dummy_industry = self.get_industry_exposure(stocks)
+                    x = dummy_industry.T
+                result[factor] = sm.OLS(y.astype(float), x.astype(float)).fit().resid
+        return result
 
     '''为股票池添加行业标记'''
-    def get_industry_exposure(stock_list):
+    def get_industry_exposure(self, stock_list):
         df = pd.DataFrame(index = (get_industries(name='sw_l1').index), columns=stock_list)
         for stock in stock_list:
             try:
@@ -148,7 +196,7 @@ class private_tools():
         return df.fillna(0)  # 将NaN赋为0
 
     '''查询个股所在行业函数代码（申万一级） ,为中性化函数的子函数'''
-    def get_industry_code_from_security(security, date=None):
+    def get_industry_code_from_security(self, security, date=None):
         industry_index = get_industries(name='sw_l1').index
         for i in range(0, len(industry_index)):
             try:
@@ -157,4 +205,5 @@ class private_tools():
             except:
                 continue
         return u'未找到'
+    
     
